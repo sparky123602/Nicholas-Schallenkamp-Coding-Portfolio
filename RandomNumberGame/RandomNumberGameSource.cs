2eedb16b7b1bb1bcced8f02082d925104f692ce0
sparky123602 <122409546+sparky123602@users.noreply.github.com>
2024-07-06 20:53:12 -0500

    Add files via upload

---
+++ RandomNumberGame/RandomNumberGameSource.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Random a = new Random();
+
+        int b = a.Next(6);
+
+        Console.WriteLine("Guess the number 1 - 5: {0}", b);
+
+        for (int i = 1; i <= 5; i++)
+        {
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            if (i >= 5)
+            {
+                Console.WriteLine("\nYou ran out of attempts. Better luck next time!");
+            }
+
+            else if (guess < b)
+            {
+                Console.WriteLine("\nYour guess was to low!!!\n");
+
+            }
+            else if (guess > b)
+            {
+                Console.WriteLine("\nYour guess was to high!!!\n");
+
+            }
+
+            else if (guess == b)
+            {
+                Console.WriteLine("\nCONGRATUALTIONS YOU WIN!!!!");
+
+                break;
+            }
+
+            
+
+        }
+
+        
+
+
+    }
+
+ }
