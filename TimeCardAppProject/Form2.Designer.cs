859c4fd7b9fc0fa09fd94d40a179cae32d74c6ba
sparky123602 <122409546+sparky123602@users.noreply.github.com>
2024-07-11 21:45:14 -0500

    Add files via upload
    This is an ongoing personal project. It is a C# Time Card Forms Application! It's main purpose, is to keep track of Clock In and Clock Out times of employees at any workplace! I used an SQL server and SQL database to store all the employees information, clock in and out times, etc. I have included an example of the table I created, that was exported into a text file from the SQL database. As well as an example of the Query commands I used to navigate my table and database contents. Going forward I would like to add a second form that is for Employer use that can help them with payroll information. More files will be added as I finish the project.

---
+++ TimeCardAppProject/Form2.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TimeCardAppProject
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(433, 400);
+            button1.Name = "button1";
+            button1.Size = new Size(143, 35);
+            button1.TabIndex = 0;
+            button1.Text = "Go To Employee Page";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(588, 447);
+            Controls.Add(button1);
+            Name = "Form2";
+            Text = "f";
+            Load += Form2_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
